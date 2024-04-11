d8f624f942312e45f221ab0386aa6aa852afe207
[asdfwefsf] <bethel5308@naver.com>
2024-04-11 15:01:42 +0900

    add data module

--- app/src/main/java/com/company/dolshop/MainActivity.kt
+++ app/src/main/java/com/company/dolshop/MainActivity.kt
@@ -17,31 +17,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DolShopTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DolShopTheme {
-        Greeting("Android")
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,4 +15,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "DolShop"
 include(":app")
- 
+include(":data")
