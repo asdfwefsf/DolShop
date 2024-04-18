8dec61fe48f56ea6faafa8c9f63fbbcbd9fd70f5
[asdfwefsf] <bethel5308@naver.com>
2024-04-18 23:59:57 +0900

    start get infokakaoauth sujeong

--- data/src/main/java/com/company/data/repositoryimpl/getUserKakaoInfoRepositoryImpl.kt
+++ data/src/main/java/com/company/data/repositoryimpl/getUserKakaoInfoRepositoryImpl.kt
@@ -33,6 +33,7 @@ class getUserKakaoInfoRepositoryImpl @Inject constructor(
 
 
 
+
     // MutableStateFlow<List<String>>
     override suspend fun getUserKakaoInfo(list: MutableStateFlow<List<String>>) {
 //        var userInfo = emptyList<String>()
