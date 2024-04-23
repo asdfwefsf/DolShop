c4ae0bee5d3dc6431aac76746cdf77c1882e3fc3
[asdfwefsf] <bethel5308@naver.com>
2024-04-23 23:58:38 +0900

    start test BaseProduct Screen

--- presentation/src/main/java/com/company/dolshop/screens/screentype/productscreen/ProductScreen.kt
+++ presentation/src/main/java/com/company/dolshop/screens/screentype/productscreen/ProductScreen.kt
@@ -1,15 +1,24 @@
 package com.company.dolshop.screens.screentype.productscreen
 
+import android.util.Log
+import androidx.compose.animation.core.FastOutLinearInEasing
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
@@ -18,9 +27,13 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.TransformOrigin
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -34,7 +47,9 @@ import coil.request.ImageRequest
 import com.company.dolshop.designsystem.DolShopTheme
 import com.company.dolshop.viewmodel.getProductViewModel
 import com.company.domain.model.DomainProductModel
+import kotlin.math.absoluteValue
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ProductScreen(innerPadding: PaddingValues) {
     val getProductViewModel: getProductViewModel = hiltViewModel()
@@ -46,8 +61,44 @@ fun ProductScreen(innerPadding: PaddingValues) {
             // add BaseProduct Screen
 
             firstBaseScreen()
+            Spacer(Modifier.padding(20.dp))
 
             // add BaseProduct Screen
+
+            // add test
+            val horizontalPagerState = rememberPagerState(
+                pageCount = {
+                    10
+                },
+                initialPage = 0
+
+            )
+            HorizontalPager(
+                state = horizontalPagerState,
+                modifier = Modifier.size(250.dp),
+            ) { page ->
+                Box(
+                    modifier = Modifier
+                        .size(250.dp)
+                        .applyCubic(horizontalPagerState, page)
+                ) {
+                    AsyncImage(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(test[page])
+                            .crossfade(true)
+                            .build(),
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .clickable { Log.d("haha", "haha") }
+                    )
+                    LaunchedEffect(key1 = horizontalPagerState.currentPage) {
+                        imageLinkViewModel.save(horizontalPagerState.currentPage)
+                    }
+                }
+            }
+            // add test
 
             LazyColumn(
                 modifier = Modifier.padding(innerPadding),
@@ -165,6 +216,45 @@ fun productItemScreen(product: DomainProductModel, onClick: (category: String) -
         )
     }
 }
+
+// add test
+
+@OptIn(ExperimentalFoundationApi::class)
+fun Modifier.applyCubic(state: PagerState, page: Int, horizontal: Boolean = true): Modifier {
+    return graphicsLayer {
+        val pageOffset = state.offsetForPage(page)
+        val offScreenRight = pageOffset < 0f
+        val def = if (horizontal) {
+            105f
+        } else {
+            -90f
+        }
+        val interpolated = FastOutLinearInEasing.transform(pageOffset.absoluteValue)
+        val rotation = (interpolated * if (offScreenRight) def else -def).coerceAtMost(90f)
+        if (horizontal) {
+            rotationY = rotation
+        } else {
+            rotationX = rotation
+        }
+
+        transformOrigin = if (horizontal) {
+            TransformOrigin(
+                pivotFractionX = if (offScreenRight) 0f else 1f,
+                pivotFractionY = .5f
+            )
+        } else {
+            TransformOrigin(
+                pivotFractionY = if (offScreenRight) 0f else 1f,
+                pivotFractionX = .5f
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+fun PagerState.offsetForPage(page: Int) = (currentPage - page) + currentPageOffsetFraction
+
+// add test
 
 @Preview
 @Composable
